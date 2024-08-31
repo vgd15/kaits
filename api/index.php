c0c75b34bdb7615ed0d7ca5523de8c212700052d
vgd15 <deise171315@gmail.com>
2024-08-31 18:22:10 -0300

    commit

--- api/index.php
+++ api/index.php
@@ -1,1 +1,41 @@
-api api/index.php img connect.php index.html script.js style.css
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$serverName = "192.168.0.64"; // Substitua pelo IP do servidor SQL
+$connectionOptions = array(
+    "Database" => "master", // Substitua pelo nome do seu banco de dados
+    "Uid" => "DeiseDuarte", // Nome do usuário
+    "PWD" => "rd232515" // Senha do usuário
+);
+
+// Conectar-se ao banco de dados
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// Verificar a conexão
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+// Verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $nascimento = $_POST['data-nascimento'];
+
+    // Preparar a consulta SQL
+    $sql = "INSERT INTO InformacoesNameDate (nome, nascimento) VALUES (?, ?)";
+    $params = array($nome, $nascimento);
+
+    // Executar a consulta
+    $stmt = sqlsrv_query($conn, $sql, $params);
+
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    } else {
+        echo "Dados inseridos com sucesso!";
+    }
+
+    // Fechar a conexão
+    sqlsrv_close($conn);
+}
+?>
